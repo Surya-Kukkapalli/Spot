dd484163c419d9ddfc8d37493dd0d7a8ccc58a8e
Surya Kukkapalli <suryakukkapalli.1@gmail.com>
2025-01-17 13:09:52 -0800

    Initial Commit

---
+++ SpotTests/SpotTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotTests.swift
+//  SpotTests
+//
+//  Created by Surya Kukkapalli on 1/17/25.
+//
+
+import Testing
+@testable import Spot
+
+struct SpotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
